dcdbf676576ec49914026880ea04f08374d7b338
Mártha Gábor <gabormartha.mail@gmail.com>
2021-06-13 11:03:45 +0200

    Update webdriver_test.py

--- testproject/webdriver_test.py
+++ testproject/webdriver_test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-driver.get("https://www.python.org")
+driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
 elem.clear()
