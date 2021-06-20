4062cd513190c20d62cccf34fe863d8c96e36a40
gofreeka <gabormartha.mail@gmail.com>
2021-06-20 15:20:19 +0200

    substract.py added

---
+++ python-homework/substract.py
@@ -0,0 +1,6 @@
+number_one = int(input("Please enter the first number:"))
+number_two = int(input("Please enter the second number:"))
+
+result = (number_one - number_two)
+
+print("The result is: " + str(result))
